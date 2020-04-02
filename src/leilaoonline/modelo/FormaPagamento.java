8585f30ece1271c4a58384ad5caa0fc86f7c29aa
Tarley Lana <tarley.lana@gmail.com>
2020-04-01 21:06:18 -0300

    feat: Cria a classe FormaPagamento
    Além dos atributos ela foi encapsulada.

---
+++ src/leilaoonline/modelo/FormaPagamento.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package leilaoonline.modelo;
+
+import java.util.Calendar;
+import java.util.Objects;
+
+/**
+ *
+ * @author Tarley
+ */
+public class FormaPagamento {
+ 
+    private String nome;
+    private String numero;
+    private Calendar criadoEm;
+    
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the numero
+     */
+    public String getNumero() {
+        return numero;
+    }
+
+    /**
+     * @param numero the numero to set
+     */
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    /**
+     * @return the criadoEm
+     */
+    public Calendar getCriadoEm() {
+        return criadoEm;
+    }
+
+    /**
+     * @param criadoEm the criadoEm to set
+     */
+    public void setCriadoEm(Calendar criadoEm) {
+        this.criadoEm = criadoEm;
+    }
+    
+    
+}
